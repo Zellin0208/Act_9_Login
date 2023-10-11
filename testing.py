15ea4f11f673c210f991e389fc362c2e5f0cd050
NeekoMiggy <143568422+NeekoMiggy@users.noreply.github.com>
2023-10-12 04:06:21 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,39 +1,42 @@
 import unittest
-from flask import url_for
 from app import login, app
+
+
 
 class TestLogin(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-    def test_login_empty_input(self):
-        username = ''
-        password = ''
+    # This test case pass.
+    def test_login_success(self):
+        username = 'admin'
+        password = 'secret'
+        response = self.app.post('/', data=dict(username=username, password=password), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Welcome to the dashboard!', response.data)
+
+    def test_login_incorrect_input(self):
+        username = '123'
+        password = '456'
+        response = self.app.post('/', data=dict(username=username, password=username), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Invalid credentials', response.data)
+
+    def test_login_incorrect_username(self):
+        username = 'adminn'
+        password = 'secret'
         response = self.app.post('/', data=dict(username=username, password=password), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Invalid credentials', response.data)
 
-    def test_login_empty_username(client):
+    def test_login_incorrect_password(self):
         username = 'admin'
-        password = ''
-        response = client.post('/', data=dict(username=username, password=password), follow_redirects=True)
-        assert response.status_code == 200
-        assert b'Invalid credentials' in response.data
+        password = 'secrete'
+        response = self.app.post('/', data=dict(username=username, password=password), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Invalid credentials', response.data)
 
-    def test_login_non_existent_username(client):
-        username = ''
-        password = 'secret'
-        response = client.post('/', data=dict(username=username, password=password) follow_redirects=True)
-        assert response.status_code == 200
-        assert b'Invalid credentials' in response.data
 
-    def test_login_correct_password(client):
-        username = 'admin'
-        password = 'secret'
-        response = client.post('/', data=dict(username=username, password=password) follow_redirects=True)
-        assert response.status_code == 200
-        assert b'Welcome to the Dashboard!' in response.data
-
-if __name__ == '__main__':
+if __name__ == 'main':
     unittest.main()
