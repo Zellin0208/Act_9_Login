edc4f43e9a564873a3c076621191fe3ee8140891
Zellin0208 <127974044+Zellin0208@users.noreply.github.com>
2023-10-11 22:56:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -6,12 +6,14 @@ def login():
         username = request.form['username']
         password = request.form['password']
         if username == 'admin' and password == 'secret':
-            return render_template('welcome.html')
+            return redirect(url_for("dashboard"))
         else:
             return render_template('index.html', error='Invalid credentials')
     else:
         return render_template('index.html')
+        
+@app.route("/dashboard")
+def dashboard():
+    return "Welcome to the dashboard!"
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    
